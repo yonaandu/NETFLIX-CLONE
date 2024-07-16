3531b9b3d4d440558507636169c67e54b6e9b44c
Yonatan Andualem Moges <andualemyonatan59@gmail.com>
2024-07-16 18:50:31 -0400

    banner

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -19,36 +19,47 @@ const Footer = () => {
         <div className='footerlist1'>
             <ul>
                 <li><a href='#'>Audio Description</a></li>
+                <br></br>
                 <li><a href='#'>Investor Relations</a></li>
+                <br></br>
                 <li><a href='#'>Privacy</a></li>
+                <br></br>
                 <li><a href='#'>Contact Us</a></li>
             </ul>
         </div>
         <div className='footerlist2'>
             <ul>
             <li><a href='#'>Help Center</a></li>
+            <br></br>
             <li><a href='#'>Jobs</a></li>
+            <br></br>
             <li><a href='#'>Legal Notices</a></li>
+            <br></br>
             <li><a href='#'>Ad Choices</a></li>
             </ul>
         </div>
         <div className='footerlist3'>
             <ul>
             <li><a href='#'>Gift Cards</a></li>
+            <br></br>
             <li><a href='#'>Netflix Shop</a></li>
+            <br></br>
             <li><a href='#'>Cookie Preferences</a></li>
             </ul>
         </div>
         <div className='footerlist4'>
             <ul>
             <li><a href='#'>Media Center</a></li>
+            <br></br>
             <li><a href='#'>Terms of Use</a></li>
+            <br></br>
             <li><a href='#'>Corporate Information</a></li>
             </ul>
         </div>
       </div>
       <br></br>
       <div className='footerservicecode'><button>Service Code</button></div>
+      <br></br>
       <div className='footerlast'>@1977-2024 Netflix,Inc.</div>
       
     </div>
--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -9,29 +9,35 @@ import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Header = () => {
   return (
     <div className='headercontainer'>
-<div className='headerleft'>
-    <ul>
-        <li className='imagelogo'><img src={Netflixlogo} width="100"></img></li>
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>TV Shows</a></li>
-        <li><a href='#'>Movies</a></li>
-        <li><a href='#'>New&Popular</a></li>
-        <li><a href='#'>My List</a></li>
-        <li><a href='#'>Browse by Language</a></li>
+      <div className='headerinsidecontainer'>
+        <div className='headerleft'>
+ 
+  
+ <ul>
 
-        
-    </ul>
+      <li className='Logo1'><img src={Netflixlogo}></img></li>
+      <br></br>
+       <li><a  href=''>Home</a></li>
+       <li><a href=''>TV Shows</a></li>
+       <li><a href=''>Movies</a></li>
+       <li><a href=''>New&Popular</a></li>
+       <li><a href=''>My List</a></li>
+       <li><a href=''>Browse by Language</a></li>
+
+       
+   </ul>
 
 </div>
 <div className='headerright'>
-  <ul>
-    <li><a href='#'><SearchIcon/></a></li>
-    <li><a href='#'>Kids</a></li>
-    <li><a href='#'><NotificationsNoneIcon /></a></li>
-     <li><a href='#'><AccountBoxIcon/></a></li>
-    <li><a href='#'><ArrowDropDownIcon/></a></li>
-  </ul>
-</div>
+ <ul>
+   <li><a href='#'><SearchIcon/></a></li>
+   <li><a href='#'>Kids</a></li>
+   <li><a href='#'><NotificationsNoneIcon /></a></li>
+    <li><a href='#'><AccountBoxIcon/></a></li>
+   <li><a href='#'><ArrowDropDownIcon/></a></li>
+ </ul>
+</div></div>
+
 
 
 
